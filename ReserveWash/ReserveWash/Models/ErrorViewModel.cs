057e9ad06b558d4d409f03a3a95387a7d2dd8d2b
Reza Rajaei <rajaeireza428@gmail.com>
2024-12-08 21:12:04 +0330

    Init

---
+++ ReserveWash/ReserveWash/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ReserveWash.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
